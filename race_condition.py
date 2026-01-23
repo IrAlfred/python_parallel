28e49c5c016ae8c7bb64668339de6c7f4e71dc1d
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2026-01-23 15:00:04 +0200

    Exemple race condition

---
+++ race_condition.py
@@ -0,0 +1,26 @@
+import threading
+import time
+nb = 0
+
+def compteur():
+    global nb
+    
+    for _ in range(100):
+        valeur_actuelle = nb
+        time.sleep(0.0001)
+        nb = valeur_actuelle + 1
+
+if __name__ == "__main__":
+    for _ in range(5):
+        compteur()
+    print(f"valeur avec exécution séquentielle : {nb}")
+
+    nb = 0
+    threads = []
+    for _ in range(5):
+        t = threading.Thread(target=compteur)
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()
+    print(f"valeur avec exécution parallèle : {nb}")
