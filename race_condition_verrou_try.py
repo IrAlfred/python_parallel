3016b5e6b615830ca6f506d47950a2df3a21ac3d
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2026-01-23 15:37:38 +0200

    Lock exemple with try...finally

---
+++ race_condition_verrou_try.py
@@ -0,0 +1,34 @@
+import threading
+import time
+nb = 0
+
+verrou = threading.Lock()
+
+def compteur():
+    global nb
+    
+    for _ in range(100):
+        verrou.acquire()
+        try:
+            valeur_actuelle = nb
+            time.sleep(0.0001)
+            nb = valeur_actuelle + 1
+        except:
+            pass
+        finally:
+            verrou.release()
+
+if __name__ == "__main__":
+    for _ in range(5):
+        compteur()
+    print(f"valeur avec exécution séquentielle : {nb}")
+
+    nb = 0
+    threads = []
+    for _ in range(5):
+        t = threading.Thread(target=compteur)
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()
+    print(f"valeur avec exécution parallèle : {nb}")
